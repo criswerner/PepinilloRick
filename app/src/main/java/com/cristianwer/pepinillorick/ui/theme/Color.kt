fc0afc6be8c983e7126740a9bcc8fb7cbea5f47e
criswerner <cristianwer@gmail.com>
2026-08-15 13:26:10 -0300

    First commit

--- app/src/main/java/com/cristianwer/pepinillorick/ui/theme/Color.kt
+++ app/src/main/java/com/cristianwer/pepinillorick/ui/theme/Color.kt
@@ -2,6 +2,17 @@ package com.cristianwer.pepinillorick.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Rick & Morty Palette
+val RickGreen = Color(0xFF97CE4C)
+val RickBlue = Color(0xFF00B0C8)
+val MortyYellow = Color(0xFFF0E14A)
+
+// Dark Theme Colors
+val DeepSpace = Color(0xFF24282F)
+val DarkSurface = Color(0xFF3C3E44)
+val OffWhite = Color(0xFFF5F5F5)
+
+// Keeping original for compatibility if needed, but we'll use the new ones
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
