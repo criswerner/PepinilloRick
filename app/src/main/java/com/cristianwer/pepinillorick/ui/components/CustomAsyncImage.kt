4a01f1a1fa4fdca1d166717d0b6614286d1af9ad
criswerner <cristianwer@gmail.com>
2026-08-17 11:11:01 -0300

    Add crossfade on image

--- app/src/main/java/com/cristianwer/pepinillorick/ui/components/CustomAsyncImage.kt
+++ app/src/main/java/com/cristianwer/pepinillorick/ui/components/CustomAsyncImage.kt
@@ -3,13 +3,15 @@ package com.cristianwer.pepinillorick.ui.components
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.cristianwer.pepinillorick.R
 
 /**
- * A reusable image component for the Rick & Morty app that handles loading and error states
- * with a consistent placeholder.
+ * A reusable asynchronous image component that handles loading and error states
+ * with a consistent placeholder and a crossfade animation.
  *
  * @param imageUrl The URL of the image to load.
  * @param contentDescription The accessibility description for the image.
@@ -17,7 +19,7 @@ import com.cristianwer.pepinillorick.R
  * @param contentScale How to scale the image within its bounds.
  */
 @Composable
-internal fun RickAndMortyImage(
+internal fun CustomAsyncImage(
     imageUrl: String,
     contentDescription: String?,
     modifier: Modifier = Modifier,
@@ -26,7 +28,10 @@ internal fun RickAndMortyImage(
     val placeholder = painterResource(id = R.drawable.placeholder)
     
     AsyncImage(
-        model = imageUrl,
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .crossfade(true)
+            .build(),
         contentDescription = contentDescription,
         modifier = modifier,
         placeholder = placeholder,
