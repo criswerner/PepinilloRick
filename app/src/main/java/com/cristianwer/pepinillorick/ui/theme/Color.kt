697dc2399099ce7262e8dfb7d9b10353dbb67b67
criswerner <cristianwer@gmail.com>
2026-08-18 15:09:30 -0300

    Add dynamic color

--- app/src/main/java/com/cristianwer/pepinillorick/ui/theme/Color.kt
+++ app/src/main/java/com/cristianwer/pepinillorick/ui/theme/Color.kt
@@ -11,6 +11,7 @@ val MortyYellow = Color(0xFFF0E14A)
 // Portal & Neon Colors
 val PortalGreenLight = Color(0xFFB2FF59)
 val PortalGreenDark = Color(0xFF76FF03)
+val NeonFavorite = Color(0xFF00BFA5) // Softened neon emerald
 
 // Dark Theme Colors
 val DeepSpace = Color(0xFF1A1C20)
@@ -20,3 +21,8 @@ val OffWhite = Color(0xFFF5F5F5)
 // Error Colors
 val ErrorLight = Color(0xFFB00020)
 val ErrorDark = Color(0xFFCF6679)
+
+// Translucent Variations
+val TranslucentBlack = Color(0x80000000) 
+val TranslucentWhite = Color(0x0DFFFFFF) 
+val SemiTransparentBlack = Color(0xCC000000)
